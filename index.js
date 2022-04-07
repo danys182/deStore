481392d4feef8a72f9e03995758ec2123f369cda
Dany Schmidt <dnl1802@gmail.com>
2022-04-07 13:15:48 -0300

    Commit inicio de proyecto

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const { send } = require('express/lib/response');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Inicio deStore');
+	res.send('Inicio deStore');
 });
 
 app.get('/products', (req, res) => {
-    res.json({
-        products: [{ name: 'Products 1' }, { name: 'Products 2' }],
-    });
+	res.json({
+		products: [{ name: 'Products 1' }, { name: 'Products 2' }],
+	});
 });
 
 app.listen(port, () => {
-    console.log('Corriendo');
+	console.log('Corriendo');
 });
